99b3687ac45d18b3ece8c16138652488f1466275
vishalhandoo <vishalhandoo067@gmail.com>
2023-01-18 10:14:32 +0530

    oop's completed

---
+++ OOP's/abstaction.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*Abstraction means showing essential data and hiding the details*/
+
+
+
+
+
+
+int main(){
+    int num=8;
+    long long power = pow(num,3);/*no need to know the defination of power function*/
+    cout<<power;
+}
---
+++ OOP's/encapsulation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*
+    Encapsulation-> it is defined as the binding together the data and functions.
+    eg->tech,sales,accounts dept.
+*/
+
+
+int main(){
+
+    return 0;
+}
